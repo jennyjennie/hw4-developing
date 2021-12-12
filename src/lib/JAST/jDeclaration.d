150f84fd86c2bbb56e3004df53b1b95780e9fc81
jennyjennie <jennyho0221@gmail.com>
2021-12-12 21:58:20 +0800

    print array type undersubscript in error message unfinished

--- src/lib/JAST/jDeclaration.d
+++
@@ -1,2 +0,0 @@
-lib/JAST/jDeclaration.o: lib/JAST/jDeclaration.cpp \
- include/JAST/jast_internal.h include/JAST/jast.h
--- src/lib/JAST/jLiteral.d
+++
@@ -1,2 +0,0 @@
-lib/JAST/jLiteral.o: lib/JAST/jLiteral.cpp include/JAST/jast_internal.h \
- include/JAST/jast.h
